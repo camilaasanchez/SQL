4f9e3b5e5d90d708aa8e7245bd23714562aeda71
Camila <73431455+camilaasanchez@users.noreply.github.com>
2026-09-15 20:27:54 +0000

    create new table

---
+++ 3- New table/16_create_table.sql
@@ -0,0 +1,13 @@
+-- SQL CODÉDEX - AGREGGATES
+-- Create a table for companies
+
+CREATE TABLE companies (
+  id INTEGER,
+  name TEXT,
+  headquarters TEXT,
+  year INTEGER
+);
+
+
+SELECT *
+FROM companies;
