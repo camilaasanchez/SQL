0789fabf4f79e7fac587d2722ade9c91e666eac1
Camila <73431455+camilaasanchez@users.noreply.github.com>
2026-09-16 19:34:34 +0000

    insert into

---
+++ 3- New table/17_insert_into.sql
@@ -0,0 +1,14 @@
+-- SQL CODÉDEX - AGREGGATES
+-- Create a table for companies
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (1, 'Twitter', 'San Francisco 🌁', 2006);
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (2, 'Duolingo', 'Pittsburgh 🐝', 2011);
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (3, 'BeReal', 'Paris 🇫🇷', 2020);
+
+INSERT INTO companies (id, name, headquarters, year)
+VALUES (4, 'Codedex', 'New York 🗽', 2022);
